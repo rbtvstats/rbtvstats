1cb1f537569b656b44b04677bace7f6a532b17b2
rbtvstats <rbtvstats@gmx.de>
2016-12-15 13:30:14 +0100

    update live metadata script

--- live/update_live_data.py
+++ live/update_live_data.py
@@ -8,6 +8,7 @@ import glob
 DATA_REPO_DIR = '/home/marcel/rbtv/rbtvdata/'
 LIVE_DATA_DIR = 'live/'
 LIVE_METADATA_FILEPATH = 'live/metadata.json'
+LIVE_METADATA_FILEPATH2 = 'live/metadata2.json'
 
 def updateMetadata():
     files = sorted(glob.glob(os.path.join(LIVE_DATA_DIR, '*.csv')))
@@ -42,11 +43,38 @@ def updateMetadata():
     with open(filepath, 'w') as file:
         json.dump(metadata, file, indent='\t')
 
+    #################################
+    #metadata 2
+    metadataFiles = []
+    totalSize = 0
+    for filepath in files:
+        with open(filepath, 'r') as file:
+            f = {}
+            f['filename'] = os.path.basename(filepath)
+            lines = file.readlines()
+            firstEntry = lines[0]
+            f['start'] = int(firstEntry.split(',')[0])
+            lastEntry = lines[-1]
+            f['end'] = int(lastEntry.split(',')[0])
+            metadataFiles.append(f)
+            totalSize += file.tell()
+
+    metadataFiles.sort(key=lambda f: f['start'])
+
+    metadata = {}
+    metadata['update'] = currentTimestamp
+    metadata['files'] = metadataFiles
+    metadata['size'] = totalSize
+
+    filepath = LIVE_METADATA_FILEPATH2
+    with open(filepath, 'w') as file:
+        json.dump(metadata, file, indent=2)
+
 if __name__ == "__main__":
     os.chdir(DATA_REPO_DIR)
 
-    os.system('git pull')
+    #os.system('git pull')
     updateMetadata()
-    os.system('git add %s' % LIVE_DATA_DIR)
-    os.system('git commit -m "update live data"')
-    os.system('git push')
+    #os.system('git add %s' % LIVE_DATA_DIR)
+    #os.system('git commit -m "update live data"')
+    #os.system('git push')
